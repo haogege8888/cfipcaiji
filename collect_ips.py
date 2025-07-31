7c8c546f45897bd30a0cee23f41bde3cf0324934
haoge <45056848+haogege8888@users.noreply.github.com>
2025-08-01 00:57:24 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -6,6 +6,7 @@ import os
 # 目标URL列表
 urls = [
     'https://ip.164746.xyz', 
+    'https://ip.haogege.xyz/',
     'https://cf.090227.xyz',
     'https://ct.090227.xyz',
     'https://cmcc.090227.xyz',    
